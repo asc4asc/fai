b562144f6db1c43573142e609ec2bbd7b851d621
Andreas <asc4asc@users.noreply.github.com>
2021-02-16 16:27:22 +0100

    Update faiserver.bash

--- faiserver.bash
+++ faiserver.bash
@@ -2,7 +2,11 @@
 sudo apt install qemu-utils bridge-utils qemu-kvm qemu-system-gui
 
 ping -c 1 google.com || exit 1
-INET="enp8s0" # better probe for interface to the internet (default gw?) 
+# INET="enp8s0" # better probe for interface to the internet (default gw?) 
+# INET="enx00e04c68353b" # 
+INET=" wlp6s0 " # Wlan laptop
+# INET="enp4s0" # SC5 Port unten  
+# INET="enp0s31f6" # SC5 Port oben
 
 USERNAME="demo"
 GROUPNAME="demo"
