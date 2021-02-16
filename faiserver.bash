3bd7845c77eb0f966037041e7ceee63c5cc93ebf
Andreas <asc4asc@users.noreply.github.com>
2021-02-16 10:47:18 +0100

    Create faiserver.bash

---
+++ faiserver.bash
@@ -0,0 +1,11 @@
+apt-get install qemu-system-gui qemu-kvm qemu-utils bridge-utils
+
+sudo mkdir /srv/fai
+sudo cp -ar config/ /srv/fai/
+sudo ( cd /srv/fai/config/basefiles/ ./mk-basefile -d . BUSTER64 )
+cl='AMD64,FAIBASE,DEBIAN,BUSTER64,DEMO,FAISERVER,GRUB_PC'
+sudo fai-diskimage -v -S10G --hostname faiserver -c$cl fai-server-buster64.qcow2
+sudo chown debian.debian fai-server-buster64.qcow2
+sudo chown demo.demo fai-server-buster64.qcow2
+sudo fai-mk-network -i enp8s0 demo
+fai-kvm disk fai-server-buster64.qcow2
