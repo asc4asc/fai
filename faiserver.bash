eb9fa4cae1266249f449898524e7f28dcb13eb07
Andreas <asc4asc@users.noreply.github.com>
2021-02-16 11:22:31 +0100

    Update faiserver.bash

--- faiserver.bash
+++ faiserver.bash
@@ -1,8 +1,10 @@
-apt-get install qemu-system-gui qemu-kvm qemu-utils bridge-utils
+#!/bin/bash
+apt install qemu-utils bridge-utils qemu-kvm qemu-system-gui
 
 sudo mkdir /srv/fai
 sudo cp -ar config/ /srv/fai/
-sudo /srv/fai/config/basefiles/mk-basefile -d . BUSTER64 
+sudo /srv/fai/config/basefiles/mk-basefile -d . BUSTER64
+sudo cp BUSTER* /srv/fai/config/basefiles/ 
 cl='AMD64,FAIBASE,DEBIAN,BUSTER64,DEMO,FAISERVER,GRUB_PC'
 sudo fai-diskimage -v -S10G --hostname faiserver -c$cl fai-server-buster64.qcow2
 sudo chown demo.demo fai-server-buster64.qcow2
