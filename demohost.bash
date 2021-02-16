da24efc92cff2a536ffcd050ba93e37f9a19cc2c
Andreas <asc4asc@users.noreply.github.com>
2021-02-16 15:01:21 +0100

    Update demohost.bash

--- demohost.bash
+++ demohost.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt install qemu-utils bridge-utils qemu-kvm qemu-system-gui
 
-ping -c 1 google.com || exit 1
 INET="enp8s0" # better probe for interface to the internet (default gw?) 
 
 USERNAME="demo"
@@ -17,5 +16,7 @@ sudo cp BUSTER* /srv/fai/config/basefiles/
 cl='AMD64,FAIBASE,DEBIAN,BUSTER64,DEMO,GRUB_PC'
 sudo fai-diskimage -v -S10G --hostname demohost -c$cl $COMPFILENAME
 sudo chown $USERNAME.$GROUPNAME $COMPFILENAME
+
+ping -c 1 google.com || exit 1
 sudo fai-mk-network -P -i $INET $USERNAME
 fai-kvm disk $COMPFILENAME
