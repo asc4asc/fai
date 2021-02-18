3ba2225e2cac3f8cb6f5f2a8cd05775e6c73f658
Andreas Schockenhoff <asc@gmx.li>
2021-02-18 15:33:22 +0000

    update now work mostly

--- gnomehost.bash
+++ gnomehost.bash
@@ -6,9 +6,9 @@ sudo apt install qemu-utils bridge-utils qemu-kvm qemu-system-gui
 
 # INET="enp8s0" # better probe for interface to the internet (default gw?) 
 # INET="enx00e04c68353b" # usb geht nicht in bridge!
-INET=" wlp6s0 " #wlan geht nicht mit bridge!
+# INET=" wlp6s0 " #wlan geht nicht mit bridge!
 # INET="enp4s0" # SC5 Port unten  
-# INET="enp0s31f6" # SC5 Port oben
+INET="enp0s31f6" # SC5 Port oben
 
 USERNAME="demo"
 GROUPNAME="demo"
@@ -18,14 +18,23 @@ COMPHOSTNAME="gnomehost"
 COMPFILENAME="fai-$COMPHOSTNAME-buster64.raw"
 cl='AMD64,DHCPC,FAIBASE,DEBIAN,BUSTER64,DEMO,EASYTEST,XORG,GNOME,GRUB_PC' # Fai Classen fuer diesen Host.
 
-sudo mkdir /srv/fai
+sudo mkdir -p /srv/fai
+sudo rm -r /srv/fai/config
 sudo cp -ar config/ /srv/fai/
-#sudo /srv/fai/config/basefiles/mk-basefile -d . BUSTER64
-#sudo cp BUSTER* /srv/fai/config/basefiles/ 
+
+if [ -f /srv/fai/config/basfile/BUSTER* ];
+then
+  echo vorhanden
+else 
+  sudo /srv/fai/config/basefiles/mk-basefile -d . BUSTER64
+  sudo cp BUSTER* /srv/fai/config/basefiles/ 
+  sudo rm BUSTER*
+fi
 sudo fai-diskimage -v -S7G --hostname $COMPHOSTNAME -c$cl $COMPFILENAME
 sudo chown $USERNAME.$GROUPNAME $COMPFILENAME
 
 ping -c 1 google.com || exit 1
 ip addr|grep $INET || exit 1
-sudo fai-mk-network -P -i $INET $USERNAME
-fai-kvm disk $COMPFILENAME
+sudo brctl show |grep tap1 >/dev/null || sudo fai-mk-network -P -i $INET $USERNAME
+fai-kvm disk $COMPFILENAME 
+
