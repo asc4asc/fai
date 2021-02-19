1ac4ca0d974028be1884797d9454f4613982997f
Andreas Schockenhoff <asc@ekf.de>
2021-02-19 11:24:39 +0100

    correct for fai-.. use in new host

--- faiserver.bash
+++ faiserver.bash
@@ -4,9 +4,9 @@ sudo apt install qemu-utils bridge-utils qemu-kvm qemu-system-gui
 ping -c 1 google.com || exit 1
 # INET="enp8s0" # better probe for interface to the internet (default gw?) 
 # INET="enx00e04c68353b" # 
-INET=" wlp6s0 " # Wlan laptop
+# INET=" wlp6s0 " # Wlan laptop
 # INET="enp4s0" # SC5 Port unten  
-# INET="enp0s31f6" # SC5 Port oben
+INET="enp0s31f6" # SC5 Port oben
 
 USERNAME="demo"
 GROUPNAME="demo"
@@ -20,7 +20,7 @@ sudo mkdir -p /srv/fai
 sudo rm -r /srv/fai/config
 sudo cp -ar config/ /srv/fai/
 
-if [ -f /srv/fai/config/basfile/BUSTER* ];
+if [ -f /srv/fai/config/basefile/BUSTER* ];
 then
   echo vorhanden
 else 
