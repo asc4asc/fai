aaacfed9923653ce30598e7b83dab68a5b87ef86
Andreas <asc4asc@users.noreply.github.com>
2021-03-17 18:20:52 +0100

    Update mount-raw.bash

--- mount-raw.bash
+++ mount-raw.bash
@@ -1,11 +1,9 @@
-# Mount fai FAIBASE raw image
+# Mount fai FAIBASE raw image CSM 
+
 losetup /dev/loop0 fai-faiserver-buster64.raw
 # fdisk -l /dev/loop0 
 # apt install kpartx
 kpartx -av /dev/loop0
-# mount -o ro /dev/mapper/loop0p1 /mnt
-# umount /mnt
-mkdir -P /target/home
-mount -o ro /dev/mapper/loop0p7 /target
-mount -o ro /dev/mapper/loop0p3 /target/home
-# umount /mnt
+mkdir -p /target/home
+mount -o ro /dev/mapper/loop0p1 /target
+mount -o ro /dev/mapper/loop0p7 /target/home
